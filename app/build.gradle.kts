04252245b6dec998b9329871d7213e766db58185
JoseJordanF <josejordanfer@gmail.com>
2024-04-15 20:47:52 +0200

    Annoying android fixes 2

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -5,7 +5,7 @@ plugins {
 
 android {
     namespace = "com.app.libraryconfigproject"
-    compileSdk = 33
+    compileSdk = 34
 
     defaultConfig {
         applicationId = "com.app.libraryconfigproject"
